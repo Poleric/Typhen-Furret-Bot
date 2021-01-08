60d8cea7449cd7bcfebf4a2fd60213e27030da84
Poleric <nixonleongkb@gmail.com>
2021-01-08 17:45:11 +0800

    Update general.py
    changed ctx.send to ctx.reply for discord.py 1.6

--- cogs/general.py
+++ cogs/general.py
@@ -23,21 +23,19 @@ class General(commands.Cog):
     async def ping(self, ctx: commands.Context):
         """Show latency"""
 
-        await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
+        await ctx.reply(f'Pong! {round(self.bot.latency * 1000)}ms')
 
     @commands.command()
     @has_permissions(manage_roles=True)
     async def prefix(self, ctx: commands.Context, prefix: str):
         """Get or set bot's prefix"""
 
-        with open('./config/server.json', 'r') as f:
-            self.server_config = json.load(f)
-            self.server_config['prefix'] = prefix
+        self.server_config['prefix'] = prefix
         with open('./config/server.json', 'w') as f:
             json.dump(self.server_config, f, indent=2)
         self.bot.command_prefix = prefix
         await self.bot.change_presence(activity=discord.Game('Pokémon | {}help'.format(self.server_config['prefix'])))
-        await ctx.send('Prefix changed to {}'.format(self.server_config['prefix']))
+        await ctx.reply('Prefix changed to {}'.format(self.server_config['prefix']))
 
 
 def setup(client):
