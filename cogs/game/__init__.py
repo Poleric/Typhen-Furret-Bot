bdf74cdb41b46d67a3d79dce96e3d2941a321928
Poleric <nixonleongkb@gmail.com>
2023-02-26 17:37:29 +0800

    Adjusted minesweeper board creation logic Switched creation logic to convolution. Slightly faster performance (not that it matters)

--- cogs/game/__init__.py
+++ cogs/game/__init__.py
@@ -1,5 +1,14 @@
 from cogs.game.minesweeper import Minesweeper
 from discord.ext import commands
+import numpy as np
+from typing import Iterator
+
+
+CHARACTER_LIMIT = 2000
+
+def translate_board_on_dict(board: np.ndarray, dictionary: dict) -> np.ndarray:
+    u, inv = np.unique(board, return_inverse=True)
+    return np.array([dictionary.get(k, k) for k in u])[inv].reshape(board.shape)
 
 
 class Game(commands.Cog):
@@ -9,19 +18,46 @@ class Game(commands.Cog):
     @commands.command()
     async def minesweeper(self, ctx, width: int = 10, height: int = 10, mines: int | float = 20, starting_row: int = None, starting_column: int = None):
         """Generate a minesweeper board"""
-        # board = Minesweeper(width, height, mines=mines, starting_tile=(starting_row, starting_column) if starting_row and starting_column else None)
-        # msg = ''
-        # character_count = 0
-        # for line in board.discord_board.split('\n'):
-        #     character_count += len(line)
-        #     if character_count < 1000:
-        #         msg += line + '\n'
-        #     else:
-        #         await ctx.send(msg)
-        #         character_count = 0
-        #         msg = line + '\n'
-        # await ctx.send(msg)
+        try:
+            board = Minesweeper(width, height, mines=mines, starting_tile=(starting_row, starting_column) if starting_row and starting_column else None)
+        except AssertionError as e:
+            await ctx.reply(str(e))
+            return
 
+        d = {
+            -1: "<:mine:892802868418084865>",
+            0: "<:0_:892802357954506872>",
+            1: "<:1_:892802370306711592>",
+            2: "<:2_:892802426594291772>",
+            3: "<:3_:892802448249487490>",
+            4: "<:4_:892802462925324319>",
+            5: "<:5_:892802477680914453>",
+            6: "<:6_:892802488707735552>",
+            7: "<:7_:892802499495485480>",
+            8: "<:8_:892802509062697030>",
+        }
+        readable_board = translate_board_on_dict(board.board, d)
+
+        def flatten_to_string(array: np.ndarray, *, wrapper: str) -> Iterator[str]:
+            width = array.shape[1]
+            for i, elem in enumerate(array.flatten(), start=1):
+                yield f"{wrapper}{elem}{wrapper}"
+                if i % width == 0:
+                    yield "\n"
+
+        final_msg = ""
+        line = ""
+        for msg in flatten_to_string(readable_board, wrapper="||"):
+            line += msg
+
+            if msg == "\n":
+                if len(final_msg) + len(line) > CHARACTER_LIMIT:
+                    await ctx.send(final_msg)
+                    final_msg = ""
+
+                final_msg += line
+                line = ""
+        await ctx.send(final_msg)
 
 async def setup(bot):
     await bot.add_cog(Game(bot))
--- cogs/game/minesweeper.py
+++ cogs/game/minesweeper.py
@@ -8,20 +8,22 @@ CoordT = tuple[int, int]
 
 class Board:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = np.zeros((height, width), dtype=np.int8)  # create a board width x height size with zeros
+        self.width: int = width
+        self.height: int = height
+        self.board: np.ndarray = np.zeros((height, width), dtype=np.int8)  # create a board width x height size with zeros
 
-    def check_in_bounds(self, coordinate: CoordT):
-        """Utility function to check if the coordinate is within the board bounds"""
+    def get_adjacent_coords(self, coordinate: CoordT) -> Iterator[CoordT]:
+        """Get the coordinates of row and column of adjacent cell given a coordinate."""
 
-        row, column = coordinate
+        for dx in range(-1, 2):  # -1, 0, 1
+            for dy in range(-1, 2):  # -1, 0, 1
+                range_x = range(self.width)  # X bounds
+                range_y = range(self.height)  # Y bounds
 
-        if row < 0 or row >= self.height:
-            return False
-        if column < 0 or column >= self.width:
-            return False
-        return True
+                new_x, new_y = coordinate[0] + dx, coordinate[1] + dy  # adjacent cell
+
+                if new_x in range_x and new_y in range_y and (dx, dy) != (0, 0):
+                    yield new_x, new_y
 
     def coords_to_number(self, coordinate: CoordT) -> int:
         """Translate a 2d coordinate of row and column starting from top right into a 1d number"""
@@ -33,22 +35,11 @@ class Board:
 
         return divmod(number, self.width)
 
-    def get_adjacent_tiles_coordinates(self, coordinate: CoordT) -> Iterator[CoordT]:
-        """Get adjacent tiles coordinate. Doesn't give coordinates from the other side of the board, exp. target is at the side of the board"""
-
-        row, column = coordinate
-
-        adjacent_coords = ((row - 1, column - 1), (row - 1, column), (row - 1, column + 1),  # top part
-                           (row, column - 1), (row, column + 1),  # side part
-                           (row + 1, column - 1), (row + 1, column), (row + 1, column + 1))  # bottom part
-
-        yield from filter(self.check_in_bounds, adjacent_coords)
-
 
 class Minesweeper(Board):
     ADJ_MINE_KERNEL = np.ones((3, 3), dtype=np.int8) * -1
 
-    def __init__(self, width: int, height: int, mines: int | float, *, starting_tile: tuple[int, int] = None):
+    def __init__(self, width: int, height: int, mines: int | float, *, starting_tile: CoordT = None):
         """Create a minesweeper board
 
         :param: width <int> - the width of the board
@@ -62,39 +53,41 @@ class Minesweeper(Board):
         if 0 < mines < 1:  # convert mine density to mines
             mines = max(1, int(mines*self.width*self.height))  # min 1 mine
 
-        assert isinstance(mines, int)  # make sure mines is an integer
+        assert isinstance(mines, int), "Mines can only be an integer, or a decimal number between 0 and 1 to represent mine density"  # make sure mines is an integer
 
         if not starting_tile:
             starting_tile = (np.random.randint(0, self.height-1), np.random.randint(0, self.width-1))
-        self.starting_tile = starting_tile
+        self.starting_tile: CoordT = starting_tile
 
         self.convol_generation(mines)
 
-    def get_randomized_coords_for_mines(self, n: int, starting_tile: tuple[int, int]) -> Iterator[CoordT]:
-        cell_num = list(range(self.width * self.height))
+    def get_randomized_coords_for_mines(self, n: int) -> Iterator[CoordT]:
+        """Get randomized coordinates for mine placements, will exclude starting position and the adjacent tiles to it."""
 
-        cell_num.remove(self.coords_to_number(starting_tile))  # exclude starting position
-        for coord in self.get_adjacent_tiles_coordinates(starting_tile):  # exclude the tiles adjacent to the starting position
+        cell_num = list(range(self.width * self.height))  # get position in 1d space
+
+        cell_num.remove(self.coords_to_number(self.starting_tile))  # exclude starting position
+        for coord in self.get_adjacent_coords(self.starting_tile):  # exclude the tiles adjacent to the starting position
             cell_num.remove(self.coords_to_number(coord))
+
+        # get random position and convert to 2d coordinates
         yield from map(self.number_to_coords, np.random.choice(cell_num, size=n, replace=False))
 
     # def generate_board(self, mines) -> None:
-    #     """Generate a board with mines and markings."""
+    #     """Generate a board with mines and markings. For loops implementation"""
     #     for coord in self.get_randomized_coords_for_mines(mines, self.starting_tile):
     #         self.board[coord] = -1
     #         for coord in self.get_adjacent_tiles_coordinates(coord):
     #             if self.board[coord] != -1:
     #                 self.board[coord] += 1
 
-    def convol_generation(self, mines) -> None:  # 5x faster than for loops generation
-        """Generate a board with mines and markings. Uses matrix convolution"""
+    def convol_generation(self, mines: int) -> None:  # 5x faster than for loops implementation, not that it really matters, it's just cool
+        """Generate a board with mines and markings. Matrix convolution implementation"""
 
         # add mines
-        for coord in self.get_randomized_coords_for_mines(mines, self.starting_tile):
+        for coord in self.get_randomized_coords_for_mines(mines):
             self.board[coord] = -1
 
-        # mark adjacent mines numbers
-        # convolute a board, then mark -1 for spot that was previously a mine
+        # marks adjacent mines numbers
+        # - convolve a board, then mark -1 for spot that was previously a mine
         self.board = np.where(self.board == -1, -1, (convolve2d(self.board, self.ADJ_MINE_KERNEL, mode="same")))
-
-
