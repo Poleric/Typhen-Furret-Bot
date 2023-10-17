b31bebb1e108b15b4206691c898a88cfd3a25f88
Poleric <nixonleongkb@gmail.com>
2023-10-17 13:07:54 +0800

    rework QOTD

--- cogs/qotd/__init__.py
+++ cogs/qotd/__init__.py
@@ -1,26 +1,66 @@
+from discord import Message, Thread
 from discord.ext import commands
+from discord.ext.commands import Bot
+import re
+
+from cogs.qotd.classes import QOTD, QOTDs
+
+THREAD_NAME_LENGTH_LIMIT = 100
+QOTD_PATTERN = r" ?QOTD[: ]"
+PIN_REASON = "QOTD"
 
 
-class QOTD(commands.Cog):
-    qotd_channel_ids = [838658959626862662,  # qotd
-                        849574569119186945]  # test
+def is_qotd(content: str) -> bool:
+    return bool(re.match(QOTD_PATTERN, content, flags=re.IGNORECASE))
 
-    def __init__(self, bot):
-        self.bot = bot
+
+def trim_string_keep_words(string: str, n_chars: int, *, seperator: str = ' ') -> str:
+    if len(string) <= n_chars:
+        return string
+
+    i = string.rfind(
+        seperator,
+        0,  # the start index
+        n_chars  # the end index, does not include itself
+    )  # find last seperator to trim off
+    assert i < n_chars  # i would always be < n_chars
+
+    if i < 0:  # the entire string is a single word.
+        return string[:n_chars]
+    return string[:i]
+
+
+async def create_qotd_thread(msg: Message) -> Thread:
+    stripped_content: str = msg.clean_content.strip()
+    if len(stripped_content) > THREAD_NAME_LENGTH_LIMIT:
+        stripped_content = trim_string_keep_words(stripped_content, THREAD_NAME_LENGTH_LIMIT - 3) + "..."
+
+    return await msg.create_thread(name=stripped_content)
+
+
+class QuestionOfTheDay(commands.Cog):
+    qotd_channel_ids = [
+        838658959626862662,  # qotd
+        958968475349569558  # test
+    ]
+
+    def __init__(self, bot: Bot):
+        self.bot: Bot = bot
+        self.pinned_qotd = QOTDs(bot=bot)
+
+    async def create_qotd(self, msg: Message):
+        """Make message QOTD and schedule removal in a day"""
+        await create_qotd_thread(msg)
+
+        await msg.pin(reason=PIN_REASON)
+        self.pinned_qotd.add(QOTD.from_message(msg))
 
     @commands.Cog.listener()
-    async def on_message(self, msg):
-        if msg.channel.id in self.qotd_channel_ids and msg.content.startswith('QOTD:'):  # check if the message is a qotd
-            # find and unpin the last qotd
-            for pinned in await msg.channel.pins():
-                if pinned.content.startswith('QOTD:'):
-                    await pinned.unpin()
-                    break
-
-            # pin the current qotd and start a thread
-            await msg.pin(reason='QOTD')
-            await msg.create_thread(name=msg.content if len(msg.content) < 100 else 'QOTD')
+    async def on_message(self, msg: Message):
+        if msg.channel.id in self.qotd_channel_ids \
+                and is_qotd(msg.content):  # check if the message is a qotd
+            await self.create_qotd(msg)
 
 
 async def setup(bot):
-    await bot.add_cog(QOTD(bot))
+    await bot.add_cog(QuestionOfTheDay(bot))
---
+++ cogs/qotd/classes.py
@@ -0,0 +1,74 @@
+import dataclasses
+from dataclasses import dataclass, field
+import asyncio
+import time
+import functools
+
+import json
+
+from typing import IO
+from discord import Message
+from discord.ext.commands import Bot
+
+A_DAY_IN_SECONDS = 10
+
+
+@dataclass(slots=True, frozen=True)
+class QOTD:
+    msg_id: int
+    channel_id: int
+    created_time: int = field(default_factory=lambda: int(time.time()))  # in unix time, from time.time()
+
+    @property
+    def end_time(self) -> int:
+        return self.created_time + A_DAY_IN_SECONDS
+
+    @classmethod
+    def from_message(cls, msg: Message):
+        return cls(
+            msg_id=msg.id,
+            channel_id=msg.channel.id,
+            created_time=int(msg.created_at.timestamp())
+        )
+
+
+class QOTDs(set):
+    """Stores tuple pair of the QOTD and its unpin task"""
+
+    def __init__(self, *args, bot: Bot, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bot: Bot = bot  # needed to fetch message from its id
+
+    async def unpin_task(self, qotd: QOTD):
+        diff = qotd.end_time - time.time()
+        await asyncio.sleep(diff)
+
+        channel = await self.bot.fetch_channel(qotd.channel_id)
+        msg: Message = await channel.fetch_message(qotd.msg_id)
+
+        await msg.unpin()
+
+    def add(self, qotd: QOTD, /) -> None:
+        assert isinstance(qotd, QOTD)
+
+        task = asyncio.create_task(self.unpin_task(qotd))
+
+        elements = (qotd, task)
+        super().add(elements)
+
+        task.add_done_callback(lambda res: self.remove(elements))
+
+    def save(self, fp: IO):
+        # TODO: might be kinda suck for performance reason
+        data = [dataclasses.asdict(d[0]) for d in self]
+        json.dump(data, fp, indent=4)
+
+    @classmethod
+    def load_from_file(cls, fp: IO, *args, **kwargs):
+        self: QOTDs = cls(*args, **kwargs)
+
+        data = json.load(fp)
+        for d in data:
+            self.add(QOTD(**d))
+
+        return self
